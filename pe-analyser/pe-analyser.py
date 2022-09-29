464303159fa6bed834a374ea643976c640b386e8
Joao Favoretti <joaopedrofavoretti@gmail.com>
2022-09-29 15:46:26 -0300

    adicionando script smot para coloracao de arvore

--- pe-analyser/pe-analyser.py
+++ pe-analyser/pe-analyser.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import shutil
 
 from pe_analyser.file_operation import remove_empty_imports, slice_filenames
 from pe_analyser.validator.valid_folder import valid_folder
@@ -24,12 +25,23 @@ if __name__== '__main__':
         print('Using C version of Damicore...')
         slice_filenames(args.folder)
         os.chdir('./damicore/bin/')
-        os.system(f'./damicore ../../{args.folder}')
+        os.system(f'./damicore -n ../../{args.folder}')
         os.chdir('../../')
+        shutil.copyfile(f'./damicore/bin/outfile-{args.folder}', f'./tree.newick')
     elif args.damicore_version == DAMICORE_VERSION_PYTHON:
         print('Using Python version of Damicore...')
         ret = os.system(f'./damicore-python/damicore.py {args.folder} --ncd-output ./damicore-python/results/ncd-matrix.phylip --format phylip --tree-output ./damicore-python/results/tree.newick --graph-image ./damicore-python/results/tree-image.png --output ./damicore-python/results/final.clusters --compress ppmd')
         if ret != 0:
             print('Error while running Damicore')
             exit(1)
-        os.system('figtree ./damicore-python/results/tree.newick')
+        shutil.copyfile('damicore-python/results/tree.newick', './tree.newick')
+
+    os.system('smot color leaf -P -p "." "#909090" -p "R-*" "#FF0000" -p "G-*" "#0000FF" tree.newick \
+                | smot color pull \
+                | smot filter --factor-by-capture="(R|G)" --some-match="R" --color="#FF0000" \
+                | smot filter --factor-by-capture="(R|G)" --some-match="G" --color="#0000FF" \
+                | smot color leaf -P -p "." "#909090"> tree.nexus')
+    
+    # os.system('figtree ./tree.nexus')
+
+    
