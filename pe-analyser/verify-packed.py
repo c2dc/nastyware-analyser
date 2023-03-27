5908a309b99fa49ef1a4a6e43077db530ac57c95
Joao F <joaopedrofavoretti@gmail.com>
2023-03-27 20:05:25 -0300

    tests

--- pe-analyser/verify-packed.py
+++ pe-analyser/verify-packed.py
@@ -3,6 +3,7 @@ import sys
 import os
 import subprocess
 import argparse
+import csv
 
 
 outs = {}
@@ -14,19 +15,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for root, dirs, files in os.walk(args.directory):
-        n = len(files)
-        for index, file in enumerate(files):
-            print('Processing file {}/{}: {}'.format(index + 1, n, file), end='\r')
-            file_path = os.path.join(root, file)
-            resp = subprocess.check_output(['file', file_path]).decode('utf-8')
-            file_type = resp.split(':')[1].strip()
-            if file_type in outs:
-                outs[file_type] += 1
-            else:
-                outs[file_type] = 1
-
-    print()
-
-    for file_type, count in outs.items():
-        print(file_type, ' ---- ', count)
+        # For each directory in the root directory
+        for dir in dirs:
+            if dir.startswith('pe-') and not dir.endswith('-f'):
+                print('\nProcessing directory: {}'.format(dir))
+                outs = {}
+                for index, file in enumerate(os.listdir(os.path.join(root, dir))):
+                    print('Processing file {}/{}: {}'.format(index + 1, len(os.listdir(os.path.join(root, dir))), file), end='\r')
+                    file_path = os.path.join(root, dir, file)
+                    resp = subprocess.check_output(['file', file_path]).decode('utf-8')
+                    file_type = resp.split(':')[1].strip()
+                    if file_type in outs:
+                        outs[file_type] += 1
+                    else:
+                        outs[file_type] = 1
+                
+                # Write the results "outs" to a csv file
+                with open(os.path.join(root, f'{dir}.csv'), 'w') as f:
+                    writer = csv.writer(f)
+                    for file_type, count in outs.items():
+                        writer.writerow([file_type, count])
 
