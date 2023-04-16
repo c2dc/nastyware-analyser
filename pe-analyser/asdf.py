d7977a71e1dc6460750738eef99e63b10f4a1abb
Joao F <joaopedrofavoretti@gmail.com>
2023-04-16 19:01:50 -0300

    reestructure repository

--- pe-analyser/asdf.py
+++
@@ -1,6 +0,0 @@
-filenames = [f'rules_{i}.yar' for i in range(1, 11)]
-
-for file in filenames:
-    content = open(file, 'r').read()
-    content = content.replace('\'', '')
-    open(file, 'w').write(content)
